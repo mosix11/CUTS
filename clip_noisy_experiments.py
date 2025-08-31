947d8528a976986a9acab8967aefbb202d4adecb
mosix11 <mosix11@yahoo.com>
2025-08-31 02:08:20 +0200

    adding visualiztion code

--- clip_noisy_experiments.py
+++ clip_noisy_experiments.py
@@ -518,7 +518,7 @@ def apply_tv(outputs_dir: Path, results_dir: Path, cfg: dict, cfg_name:str):
     results_dirs['W_norms'] = results_dir / 'weight_norms'
     results_dirs['TV_norms'] = results_dir / 'TV_norms'
     results_dirs['embed_plots'] = results_dir / 'embedding_plots'
-    results_dirs['metrics'] = results_dir / 'metrics'    
+    results_dirs['metrics'] = results_dir / 'metrics'
     for dir in results_dirs.values():
         dir.mkdir(exist_ok=True, parents=True)
     
@@ -539,7 +539,7 @@ def apply_tv(outputs_dir: Path, results_dir: Path, cfg: dict, cfg_name:str):
     dataset, num_classes = dataset_factory.create_dataset(dataset_cfg)
     
     strategy = cfg['strategy']
-    dataset.inject_noise(**strategy['noise']['pretraining'])
+    # dataset.inject_noise(**strategy['noise']['pretraining'])
 
 
 
@@ -628,19 +628,25 @@ def apply_tv(outputs_dir: Path, results_dir: Path, cfg: dict, cfg_name:str):
     )
 
 
-    # model.load_state_dict(mix_weights, strict=False)
-    # umap_plot(
+    model.load_state_dict(mix_weights, strict=False)
+    # fig_umap_pt = umap_plot(
     #     feature_extractor=model.get_image_encoder(),
     #     dataloader=dataset.get_train_dataloader(),
     #     device=gpu,
+    #     class_names=dataset.get_class_names()
     # )
     
-    # task_vectors['Average TV'].apply_to(model, scaling_coef=-1.0, strict=False)
-    # umap_plot(
-    #     feature_extractor=model.get_image_encoder(),
-    #     dataloader=dataset.get_train_dataloader(),
-    #     device=gpu,
-    # )
+    # fig_umap_pt.savefig(results_dirs['embed_plots'] / "umap_pt.png", bbox_inches="tight")
+    
+    task_vectors['Average TV'].apply_to(model, scaling_coef=-1.0, strict=False)
+    fig_umap_AVG_1 = umap_plot(
+        feature_extractor=model.get_image_encoder(),
+        dataloader=dataset.get_train_dataloader(),
+        device=gpu,
+        class_names=dataset.get_class_names()
+    )
+    
+    fig_umap_AVG_1.savefig(results_dirs['embed_plots'] / "umap_avg_tv.png", bbox_inches="tight")
     
     # exit()
     
@@ -685,404 +691,6 @@ def apply_tv(outputs_dir: Path, results_dir: Path, cfg: dict, cfg_name:str):
     
     # with open(results_dir / 'tv_metrics.json' , 'w') as json_file:
     #     json.dump(results_dict, json_file, indent=4)
-    
-def apply_tvs(outputs_dir: Path, results_dir: Path, cfg: dict, cfg_name:str):
-    training_seed = cfg['training_seed']
-    if training_seed:
-        random.seed(training_seed)
-        np.random.seed(training_seed)
-        torch.manual_seed(training_seed)
-        torch.cuda.manual_seed_all(training_seed)
-    
-    cpu = trainer_utils.get_cpu_device()
-    gpu = trainer_utils.get_gpu_device()
-    
-    
-    dataset, num_classes = dataset_factory.create_dataset(cfg)
-    
-    base_model = model_factory.create_model(cfg['model'], num_classes)
-    
-    results_dir = results_dir / cfg_name
-    results_dir.mkdir(exist_ok=True, parents=True)
-    
-    results_dirs = {}
-    results_dirs['cms'] = results_dir / 'confusion_mats'
-    results_dirs['Ts'] = results_dir / 'transition_mats'
-    results_dirs['W_norms'] = results_dir / 'weight_norms'
-    results_dirs['TV_norms'] = results_dir / 'TV_norms'
-    results_dirs['embed_plots'] = results_dir / 'embedding_plots'
-    results_dirs['metrics'] = results_dir / 'metrics'    
-    for dir in results_dirs.values():
-        dir.mkdir(exist_ok=True, parents=True)
-
-    
-    
-    
-    base_expr_dir = outputs_dir / cfg_name
-    gold_dir = base_expr_dir / 'gold'
-    pretrain_dir = base_expr_dir / 'pretrain'
-    ft_gold_dir = base_expr_dir / 'finetune_gold'
-    ft_gt_noise_dir = base_expr_dir / 'finetune_gt_noise'
-    finetune_dirs = OrderedDict()
-    for idx, noise_tv in enumerate(cfg['strategy']['noise']['finetuning']):
-        ft_expr_dir = base_expr_dir / f"finetune_{noise_tv['noise_rate']}_{noise_tv['seed']}"
-        finetune_dirs[f"{noise_tv['noise_rate']}_{noise_tv['seed']}"] = ft_expr_dir
-        
-    gold_weights = torch.load(gold_dir / 'weights/model_weights.pth', map_location=cpu)
-    pretrain_weights = torch.load(pretrain_dir / 'weights/model_weights.pth', map_location=cpu)
-    ft_gold_wieghts = torch.load(ft_gold_dir / 'weights/model_weights.pth', map_location=cpu)
-    ft_gt_noise_weights = torch.load(ft_gt_noise_dir / 'weights/model_weights.pth', map_location=cpu)
-    finetune_weights = OrderedDict()
-    for ft_expr, ft_dir in finetune_dirs.items():
-        finetune_weights[ft_expr] = torch.load(ft_dir / 'weights/model_weights.pth', map_location=cpu)
-    
- 
-    
-    weight_norm_analysis.plot_abs_weight_norms_compare(
-        state_dicts={
-            'Pretrain': pretrain_weights,
-            'Gold': gold_weights,
-            'FT Noise': next(iter(finetune_weights.items()))[1]
-            },
-        saving_path=results_dirs['W_norms'] / 'L1_pt_gold_ftnoise.png'
-    )
-    
-    
-    weight_norm_analysis.plot_abs_weight_norms_compare(
-        state_dicts={
-            'Pretrain': pretrain_weights,
-            'FT Gold': ft_gold_wieghts,
-            'FT Noise': next(iter(finetune_weights.items()))[1]
-            },
-        saving_path=results_dirs['W_norms'] / 'L1_pt_ftgold_ftnoise.png'
-    )
-    
-    weight_norm_analysis.plot_abs_weight_norms_compare(
-        state_dicts={
-            'FT Gold': ft_gold_wieghts,
-            'FT Noise': next(iter(finetune_weights.items()))[1]
-            },
-        saving_path=results_dirs['W_norms'] / 'L1_ftgold_ftnoise.png'
-    )
-    
-    weight_norm_analysis.plot_abs_weight_norms_compare(
-        state_dicts={
-            'Gold': gold_weights,
-            'Pretrain': pretrain_weights,
-            'FT Gold': ft_gold_wieghts,
-            },
-        saving_path=results_dirs['W_norms'] / 'L1_pt_gold_ftgold.png'
-    )
-    
-    weight_norm_analysis.plot_abs_weight_norms_compare(
-        state_dicts={
-            'Pretrain': pretrain_weights,
-            'FT Gold': ft_gold_wieghts,
-            'FT Noise': next(iter(finetune_weights.items()))[1],
-            'FT GT Noise': ft_gt_noise_weights
-            },
-        saving_path=results_dirs['W_norms'] / 'L1_pt_ftgold_ftnoise_gtnoise.png'
-    )
-    
-    
-    ft_gold_tv = TaskVector(pretrain_weights, ft_gold_wieghts)
-    ft_gt_noise_tv = TaskVector(pretrain_weights, ft_gt_noise_weights)
-
-    finetune_tvs = OrderedDict()
-    
-    for ft_expr, ft_weight in finetune_weights.items():
-        finetune_tvs[f"{float(ft_expr.split('_')[0])*100:.0f}% Noise, {ft_expr.split('_')[1]} Seed"] = TaskVector(pretrain_weights, ft_weight)
-        
-    if len(finetune_tvs) == 1:
-        finetune_tvs['Average TV'] = list(finetune_tvs.items())[0][1]
-        finetune_tvs.popitem(last=False)
-    else:
-        finetune_tvs['Average TV'] = TaskVector.mean(finetune_tvs)
-    finetune_tvs['Average TV Pruned 0.4'] = finetune_tvs['Average TV'].prune_small_weights(rate=0.4)
-    finetune_tvs['Average TV Pruned 0.6'] = finetune_tvs['Average TV'].prune_small_weights(rate=0.6)
-    finetune_tvs['Average TV Pruned 0.8'] = finetune_tvs['Average TV'].prune_small_weights(rate=0.8)
-    finetune_tvs['Average TV Pruned 0.9'] = finetune_tvs['Average TV'].prune_small_weights(rate=0.9)
-    finetune_tvs['Average TV Pruned 0.95'] = finetune_tvs['Average TV'].prune_small_weights(rate=0.95)
-    finetune_tvs['Average TV Pruned 0.99'] = finetune_tvs['Average TV'].prune_small_weights(rate=0.99)
-    finetune_tvs['Random Vector'] = finetune_tvs['Average TV'].generate_random_vector_with_same_layer_norms(seed=11)
-    finetune_tvs['Gold'] = ft_gold_tv
-    finetune_tvs['Ground Truth Noise'] = ft_gt_noise_tv
-    finetune_tvs.move_to_end('Ground Truth Noise', last=False)
-    finetune_tvs.move_to_end('Gold', last=False)
-    
-    
-
-    ft_tvs_list = list(finetune_tvs.values())
-    print(finetune_tvs.keys())
-    tv_names = list(finetune_tvs.keys())
-    
-    task_sim = []
-    for i in range(len(ft_tvs_list)):
-        anchor_tv = ft_tvs_list[i]
-        task_sim.append([])
-        for j in range(len(ft_tvs_list)):
-            other_tv = ft_tvs_list[j]
-            cos_sim = anchor_tv.cosine_similarity_flatten(other_tv)
-            task_sim[i].append(cos_sim)
-    task_sim = np.array(task_sim)
-    
-    misc_utils.plot_confusion_matrix(
-        title='Task Vector Similarity Matrix',
-        cm=task_sim,
-        class_names=tv_names,
-        color_map='vlag',
-        color_bar=True,
-        vmin= -1.0,
-        vmax= 1.0,
-        x_label='Task Vectors',
-        y_label='Task Vectors',
-        tick_label_font_size=6,
-        filepath=results_dir / 'task_similarities.png',
-        show=False
-    )
-    
-    
-    weight_norm_analysis.plot_abs_weight_norms_compare(
-        state_dicts={
-            'Average TV': finetune_tvs['Average TV'].vector,
-            'Gold TV': finetune_tvs['Gold'].vector,
-            # 'Average TV Pruned 0.8': finetune_tvs['Average TV Pruned 0.8'].vector
-            },
-        saving_path=results_dirs['TV_norms'] / 'L1_norms.png'
-    )
-    
-    
-    weight_norm_analysis.plot_l2_weight_norms_compare(
-        state_dicts={
-            'Average TV': finetune_tvs['Average TV'].vector,
-            'Gold TV': finetune_tvs['Gold'].vector,
-            # 'Average TV Pruned 0.8': finetune_tvs['Average TV Pruned 0.8'].vector
-            },
-        saving_path=results_dirs['TV_norms'] / 'L2_norms.png'
-    )
-    
-    
-    base_model.load_state_dict(pretrain_weights)
-
-    cm_pt = get_confusion_matrix(
-        base_model,
-        dataset.get_num_classes(),
-        dataset.get_heldout_dataloader(),
-        gpu
-    )
-    
-    # T = estimate_T_from_confusion(cm_pt, alpha=0.01, lam=0.1)
-    T= None
-    
-    misc_utils.plot_confusion_matrix(
-        title='Noise Transition Matrix',
-        cm=T,
-        class_names=dataset.get_class_names(),
-        color_map='vlag',
-        color_bar=True,
-        # vmin= 0.0,
-        # vmax= 1.0,
-        x_label='Classes',
-        y_label='Classes',
-        tick_label_font_size=6,
-        filepath=results_dirs['Ts'] / 'transition_matrix.png',
-        show=False
-    )
-    
-    
-    
-    
-    misc_utils.plot_confusion_matrix(
-        title='Normalized Confusion Matrix',
-        cm=row_normalize(cm_pt),
-        class_names=dataset.get_class_names(),
-        color_map='vlag',
-        color_bar=True,
-        # vmin= 0.0,
-        # vmax= 1.0,
-        x_label='Classes',
-        y_label='Classes',
-        tick_label_font_size=6,
-        filepath=results_dirs['cms'] / 'pretrained_normalized.png',
-        show=False
-    )
-    
-    
-    ft_model = copy.deepcopy(base_model)
-    ft_model.load_state_dict(next(iter(finetune_weights.items()))[1])
-    cm_ft = get_confusion_matrix(
-        ft_model,
-        dataset.get_num_classes(),
-        dataset.get_heldout_dataloader(),
-        gpu
-    )
-    
-    misc_utils.plot_confusion_matrix(
-        title='Normalized Confusion Matrix',
-        cm=row_normalize(cm_ft),
-        class_names=dataset.get_class_names(),
-        color_map='vlag',
-        color_bar=True,
-        # vmin= 0.0,
-        # vmax= 1.0,
-        x_label='Classes',
-        y_label='Classes',
-        tick_label_font_size=6,
-        filepath=results_dirs['cms'] / 'ft_noise_normalized.png',
-        show=False
-    )
-    
-    finetune_tvs['Average TV'].apply_to(base_model, scaling_coef=-1.0)
-    cm_ng = get_confusion_matrix(
-        base_model,
-        dataset.get_num_classes(),
-        dataset.get_heldout_dataloader(),
-        gpu
-    )
-    
-    misc_utils.plot_confusion_matrix(
-        title='Normalized Confusion Matrix',
-        cm=row_normalize(cm_ng),
-        class_names=dataset.get_class_names(),
-        color_map='vlag',
-        color_bar=True,
-        # vmin= 0.0,
-        # vmax= 1.0,
-        x_label='Classes',
-        y_label='Classes',
-        tick_label_font_size=6,
-        filepath=results_dirs['cms'] / 'negated_normalized.png',
-        show=False
-    )
-        
-
-    # rank_dict = OrderedDict()
-    # for tv_name, tv in finetune_tvs.items():
-    #     rank_dict[tv_name] = tv.get_layer_rank()
-        
-    # with open(results_dir / 'ranks.json' , 'w') as json_file:
-    #     json.dump(rank_dict, json_file, indent=4)
-    
-    
-    # for i in range(len(ft_tvs_list)):
-    #     if i == 0:
-    #         print('passing ft gold from low rank approximation')
-    #         continue
-    #     else:
-    #         ftsv = ft_tvs_list[i].compute_SVD_for_each_layer(k=0.1)
-        
-    #         ft_tvs_list[i].apply_SVD_to_TV(ftsv)
-    
-    
-    # task_sim = []
-    # for i in range(len(ft_tvs_list)):
-    #     anchor_tv = ft_tvs_list[i]
-    #     task_sim.append([])
-    #     for j in range(len(ft_tvs_list)):
-    #         other_tv = ft_tvs_list[j]
-    #         cos_sim = anchor_tv.cosine_similarity_flatten(other_tv)
-    #         task_sim[i].append(cos_sim)
-    # task_sim = np.array(task_sim)
-    
-    # misc_utils.plot_confusion_matrix(cm=task_sim, class_names=class_names, filepath=None, show=True)
-    
-    # for ft_name, ft_tv in finetune_tvs.items():
-    #     best_coef, best_results, best_cm = search_optimal_coefficient(
-    #         base_model=base_model,
-    #         task_vector=ft_tv,
-    #         search_range=(-1.5, 0.0),
-    #         dataset=dataset,
-    #         num_classes=num_classes,
-    #         device=gpu
-    #     )
-    #     print(f"Best scaling coefficient for {ft_name} = {best_coef}")
-    #     print(f"Metrics of the negated model is {best_results}")
-            
-    
-    
-
-        
-    # TSV = TaskVector.TSV_extract_common_direction(finetune_tvs, k=0.3)
-    # TSV.apply_to(base_model, scaling_coef=1.0)
-        
-    # for ft_name, ft_tv in finetune_tvs.items():
-    #     best_coef, best_results, best_cm = search_optimal_coefficient(
-    #         base_model=base_model,
-    #         task_vector=ft_tv,
-    #         search_range=(-1.5, 0.0),
-    #         dataset=dataset,
-    #         num_classes=num_classes,
-    #         device=gpu
-    #     )
-    #     print(f"Best scaling coefficient for {ft_name} = {best_coef}")
-    #     print(f"Metrics of the negated model is {best_results}")
-    
-    # test_tv = (ft_tvs_list[2] + ft_tvs_list[3]) * 0.5
-    
-    # ordered_dict = OrderedDict(zip(tv_names[1:], ft_tvs_list[1:]))
-    
-    # best_coef, best_results, best_cm = search_optimal_coefficient(
-    #     base_model=base_model,
-    #     # task_vector=test_tv,
-    #     task_vector=ft_tvs_list[4],
-    #     search_range=(-3.0, 0.0),
-    #     dataset=dataset,
-    #     num_classes=num_classes,
-    #     device=gpu
-    # )
-    
-    # print(f"Best scaling coefficient for TV = {best_coef}")
-    # print(f"Metrics of the negated model is {best_results}")
-    
-    # before_tv_metrics = eval_model_on_clean_noise_splits(base_model, cfg, dataset, gpu)
-    # print('Performance before TV:', before_tv_metrics)
-    # base_model.to(cpu)
-    # # test_tv.apply_to(base_model, scaling_coef=best_coef)
-    # ft_tvs_list[4].apply_to(base_model, scaling_coef=best_coef)
-    
-    # after_tv_metrics = eval_model_on_clean_noise_splits(base_model, cfg, dataset, gpu)
-    # print('Performance after TV:', after_tv_metrics)
-    
-    # base_model.load_state_dict(next(iter(finetune_weights.items()))[1])
-    # temp_res = eval_model_on_clean_noise_splits(base_model, cfg, dataset, gpu)
-    # print(temp_res)
-    # exit()
-    
-    base_model.load_state_dict(pretrain_weights)
-    pt_test_results, _, _ = evaluate_model(base_model, dataset.get_test_dataloader(), gpu)
-    pt_train_results = eval_model_on_clean_noise_splits(base_model, cfg, dataset, gpu)
-    
-    base_model.load_state_dict(gold_weights)
-    gold_test_results, _, _ = evaluate_model(base_model, dataset.get_test_dataloader(), gpu)
-    gold_train_results = eval_model_on_clean_noise_splits(base_model, cfg, dataset, gpu)
-    
-    base_model.load_state_dict(ft_gold_wieghts)
-    ft_gold_test_results, _, _ = evaluate_model(base_model, dataset.get_test_dataloader(), gpu)
-    ft_gold_train_results = eval_model_on_clean_noise_splits(base_model, cfg, dataset, gpu)
-    
-    base_model.load_state_dict(pretrain_weights)
-    base_model.to(cpu)
-    
-    results_dict = OrderedDict()
-    
-    results_dict['Pretrain'] = {'test_results': pt_test_results, 'train_results': pt_train_results}
-    results_dict['Gold'] = {'test_results': gold_test_results, 'train_results': gold_train_results}
-    results_dict['Finetune Gold'] = {'test_results': ft_gold_test_results, 'train_results': ft_gold_train_results}
-    results_dict = eval_model_on_tvs(base_model, OrderedDict(zip(tv_names[1:], ft_tvs_list[1:])), results_dict, cfg, dataset, num_classes, gpu)
-    # results_dict = eval_model_on_tvs(base_model, OrderedDict(zip(tv_names, ft_tvs_list)), results_dict, cfg, dataset, num_classes, gpu)
-    
-    
-    # print(results_dict)
-        
-        
-    with open(results_dirs['metrics'] / 'metrics.json' , 'w') as json_file:
-        json.dump(results_dict, json_file, indent=4)
-
-    
-
-
-
-
     
     
 
--- src/utils/embedding_space_analysis.py
+++ src/utils/embedding_space_analysis.py
@@ -6,6 +6,9 @@ import umap
 import matplotlib.pyplot as plt
 import numpy as np
 from tqdm import tqdm
+import torchdr
+from typing import List
+import datamapplot
 
 def prepare_batch(batch, device):
     return [t.to(device, non_blocking=True) for t in batch]
@@ -40,11 +43,12 @@ def extract_features(
     labels = torch.cat(labels, dim=0)
     return feats, labels
 
-# Run UMAP
+
 def umap_plot(
     feature_extractor:nn.Module,
     dataloader:DataLoader,
     device:torch.device = torch.device('cpu'),
+    class_names:List[str] = None,
     normalize:bool = False,
     n_neighbors:int = 15,
     min_dist:float = 0.1,
@@ -52,16 +56,53 @@ def umap_plot(
 ):
     features, labels = extract_features(feature_extractor, dataloader, normalize, device)
     
-    reducer = umap.UMAP(n_neighbors=n_neighbors,
-                        min_dist=min_dist,
-                        n_components=n_components,
-                        random_state=42)
-    embedding = reducer.fit_transform(features)
+
+    class_names = dict(enumerate(class_names))
+    vectorized_converter = np.vectorize(lambda x: class_names[x])
+    labels_str = vectorized_converter(labels)
+        
+    z = torchdr.UMAP(
+        n_components=n_components,
+        verbose=True,
+        n_neighbors=n_neighbors,
+        min_dist=min_dist,
+        device=device,
+        backend="keops"
+    ).fit_transform(features)
+    z = z.detach().cpu().numpy()
+        
+    fig, ax = datamapplot.create_plot(
+        z,
+        labels_str,
+        label_over_points=True,
+        label_font_size=30,
+    )
     
-    plt.figure(figsize=(8,6))
-    scatter = plt.scatter(embedding[:,0], embedding[:,1], 
-                          c=labels, cmap="tab10", s=10, alpha=0.7)
-    plt.legend(*scatter.legend_elements(), title="Classes")
-    plt.title("UMAP of Image Encoder Features")
-    plt.show()
+    return fig
+    
+
+# # Run UMAP
+# def umap_plot(
+#     feature_extractor:nn.Module,
+#     dataloader:DataLoader,
+#     device:torch.device = torch.device('cpu'),
+#     normalize:bool = False,
+#     n_neighbors:int = 15,
+#     min_dist:float = 0.1,
+#     n_components:int = 2
+# ):
+#     features, labels = extract_features(feature_extractor, dataloader, normalize, device)
+    
+#     reducer = umap.UMAP(n_neighbors=n_neighbors,
+#                         min_dist=min_dist,
+#                         n_components=n_components,
+#                         random_state=42)
+#     embedding = reducer.fit_transform(features)
+    
+#     plt.figure(figsize=(8,6))
+#     scatter = plt.scatter(embedding[:,0], embedding[:,1], 
+#                           c=labels, cmap="tab10", s=10, alpha=0.7)
+#     plt.legend(*scatter.legend_elements(), title="Classes")
+#     plt.title("UMAP of Image Encoder Features")
+#     plt.show()
 
