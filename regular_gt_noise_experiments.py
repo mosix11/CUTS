4c4306a052aa3ec9472d4106be97d098652322c2
mosix11 <mosix11@yahoo.com>
2025-09-07 19:56:19 +0200

    change cfg

--- regular_gt_noise_experiments.py
+++ regular_gt_noise_experiments.py
@@ -199,7 +199,7 @@ def finetune_models(outputs_dir: Path, results_dir: Path, cfg: dict, cfg_name:st
         
         
         
-    for discovery_rate in np.linspace(0.1, 1.0, 10):
+    for discovery_rate in np.round(np.linspace(0.1, 1.0, 10), 1):
         if not outputs_dir.joinpath(f"{cfg_name}/finetune_{discovery_rate}/weights/ft_weights.pth").exists():
             dataset = copy.deepcopy(base_dataset)
             model = copy.deepcopy(base_model)
@@ -267,17 +267,13 @@ def apply_tv(outputs_dir: Path, results_dir: Path, cfg: dict, cfg_name:str):
     dataset, num_classes = dataset_factory.create_dataset(cfg['dataset'])
     model = model_factory.create_model(cfg['model'], num_classes)
     
-    
     dataset.reset_train_dl(shuffle=False)
     
     dataset_clean = copy.deepcopy(dataset)
     
     strategy = cfg['strategy']
-    dataset.inject_poison(**strategy['poison']['pretraining'])
+    dataset.inject_noise(**strategy['noise']['pretraining'])
     
-    poison_tv_cfg = strategy['poison']['finetuning'][0]
-    poison_tv_cfg['set'] = 'Heldout'
-    dataset.inject_poison(**poison_tv_cfg)
 
 
 
@@ -292,111 +288,96 @@ def apply_tv(outputs_dir: Path, results_dir: Path, cfg: dict, cfg_name:str):
         map_location='cpu'
     )
     
-    ft_ho_clean_weights = torch.load(
-        outputs_dir.joinpath(f"finetune_clean/weights/ft_weights.pth"),
-        map_location='cpu'
-    )
     
     
-    poison_weights = OrderedDict()
+    noise_weights = OrderedDict()
     
-    for poison_tv in cfg['strategy']['poison']['finetuning']:
-        ft_expr_dir = outputs_dir / f"finetune_{poison_tv['rate']}_{poison_tv['seed']}"
+    for discovery_rate in np.round(np.linspace(0.1, 1.0, 10), 1):
+        if discovery_rate != 0.5:
+            continue
+        ft_expr_dir = outputs_dir / f"finetune_{discovery_rate}"
         n_weights = torch.load(
             ft_expr_dir.joinpath(f"weights/ft_weights.pth"),
             map_location='cpu'
         )
-        poison_weights[f"{poison_tv['rate']*100:.0f}% Noise, {poison_tv['seed']} Seed"] = n_weights
+        noise_weights[f"Discovery Rate {discovery_rate}"] = n_weights
         
-    
+        
             
     task_vectors = OrderedDict()
-    for task_name, finetuend_weights in poison_weights.items():
+    for task_name, finetuend_weights in noise_weights.items():
         task_vectors[task_name] = TaskVector(mix_weights, finetuend_weights)
-        
-    if len(task_vectors) == 1:
-        only_tv = task_vectors.popitem(last=False)[1]
-        task_vectors['Average TV'] = only_tv
-    else:
-        task_vectors['Average TV'] = TaskVector.mean(task_vectors)
-        
-    
-    task_vectors['Clean'] = TaskVector(mix_weights, ft_ho_clean_weights)
-    
-    task_vectors['Random Vector'] = task_vectors['Average TV'].generate_random_vector_with_same_layer_norms(seed=11)
-
     
     
     
-    ft_tvs_list = list(task_vectors.values())
-    tv_names = list(task_vectors.keys())
     
-    task_sim = []
-    for i in range(len(ft_tvs_list)):
-        anchor_tv = ft_tvs_list[i]
-        task_sim.append([])
-        for j in range(len(ft_tvs_list)):
-            other_tv = ft_tvs_list[j]
-            cos_sim = anchor_tv.cosine_similarity_flatten(other_tv)
-            task_sim[i].append(cos_sim)
-    task_sim = np.array(task_sim)
+    # ft_tvs_list = list(task_vectors.values())
+    # tv_names = list(task_vectors.keys())
     
-    misc_utils.plot_confusion_matrix(
-        title='Task Vector Similarity Matrix',
-        cm=task_sim,
-        class_names=tv_names,
-        color_map='vlag',
-        color_bar=True,
-        vmin= -1.0,
-        vmax= 1.0,
-        x_label='Task Vectors',
-        y_label='Task Vectors',
-        tick_label_font_size=6,
-        filepath=results_dir / 'task_similarities.png',
-        show=False
-    )
+    # task_sim = []
+    # for i in range(len(ft_tvs_list)):
+    #     anchor_tv = ft_tvs_list[i]
+    #     task_sim.append([])
+    #     for j in range(len(ft_tvs_list)):
+    #         other_tv = ft_tvs_list[j]
+    #         cos_sim = anchor_tv.cosine_similarity_flatten(other_tv)
+    #         task_sim[i].append(cos_sim)
+    # task_sim = np.array(task_sim)
+    
+    # misc_utils.plot_confusion_matrix(
+    #     title='Task Vector Similarity Matrix',
+    #     cm=task_sim,
+    #     class_names=tv_names,
+    #     color_map='vlag',
+    #     color_bar=True,
+    #     vmin= -1.0,
+    #     vmax= 1.0,
+    #     x_label='Task Vectors',
+    #     y_label='Task Vectors',
+    #     tick_label_font_size=6,
+    #     filepath=results_dir / 'task_similarities.png',
+    #     show=False
+    # )
 
 
     
 
-    model.load_state_dict(mix_weights, strict=False)
-    mix_test_results, _, _ = evaluate_model(model, dataset.get_test_dataloader(), gpu)
-    mix_ho_results, _, _ = evaluate_model(model, dataset.get_heldout_dataloader(), gpu)
-    mix_train_results = eval_model_on_clean_noise_splits(model, None, dataset, gpu)
+    # model.load_state_dict(mix_weights, strict=False)
+    # mix_test_results, _, _ = evaluate_model(model, dataset.get_test_dataloader(), gpu)
+    # mix_train_results = eval_model_on_clean_noise_splits(model, None, dataset, gpu)
     
     
-    model.load_state_dict(gold_weights, strict=False)
-    gold_test_results, _, _ = evaluate_model(model, dataset.get_test_dataloader(), gpu)
-    gold_ho_results, _, _ = evaluate_model(model, dataset.get_heldout_dataloader(), gpu)
-    gold_train_results = eval_model_on_clean_noise_splits(model, None, dataset, gpu)
+    # model.load_state_dict(gold_weights, strict=False)
+    # gold_test_results, _, _ = evaluate_model(model, dataset.get_test_dataloader(), gpu)
+    # gold_train_results = eval_model_on_clean_noise_splits(model, None, dataset, gpu)
     
-    model.load_state_dict(ft_ho_clean_weights, strict=False)
-    ft_ho_test_results, _, _ = evaluate_model(model, dataset.get_test_dataloader(), gpu)
-    ft_ho_ho_results, _, _ = evaluate_model(model, dataset.get_heldout_dataloader(), gpu)
-    ft_ho_train_results = eval_model_on_clean_noise_splits(model, None, dataset, gpu)
     
     
     results_dict = OrderedDict()
     
-    results_dict['Mix'] = {'test_results': mix_test_results, 'ho_results': mix_ho_results, 'train_results': mix_train_results}
-    results_dict['Gold'] = {'test_results': gold_test_results, 'ho_results': gold_ho_results, 'train_results': gold_train_results}
-    results_dict['FT HO Clean'] = {'test_results': ft_ho_test_results, 'ho_results': ft_ho_ho_results, 'train_results': ft_ho_train_results}
+    # results_dict['Mix'] = {'test_results': mix_test_results, 'train_results': mix_train_results}
+    # results_dict['Gold'] = {'test_results': gold_test_results, 'train_results': gold_train_results}
     
     # results_dict = OrderedDict()
     # for alpha in tqdm(np.linspace(-0.05, -1.5, 30)):
     # for alpha in tqdm(np.linspace(-0.1, -2.0, 20)):
-    for alpha in tqdm(np.linspace(-0.1, -1.5, 15)):
     
-        model.load_state_dict(mix_weights, strict=False)
-        task_vectors['Average TV'].apply_to(model, scaling_coef=alpha, strict=False)
-        tv_test_results, _, _ = evaluate_model(model, dataset.get_test_dataloader(), gpu)
-        tv_ho_resutls, _, _ = evaluate_model(model, dataset.get_heldout_dataloader(), gpu)
-        tv_train_results = eval_model_on_clean_noise_splits(model, None, dataset, gpu)
+    for discovery_rate in np.round(np.linspace(0.1, 1.0, 10), 1):
+        if discovery_rate != 0.5:
+            continue
+        results_dict[discovery_rate] = OrderedDict()
+        for alpha in tqdm(np.linspace(-0.2, -10, 15)):
+        
+            model.load_state_dict(mix_weights, strict=False)
+            task_vectors[f"Discovery Rate {discovery_rate}"].apply_to(model, scaling_coef=alpha, strict=False)
+            tv_test_results, _, _ = evaluate_model(model, dataset.get_test_dataloader(), gpu)
+            tv_train_results = eval_model_on_clean_noise_splits(model, None, dataset, gpu)
 
-        results_dict[alpha] = {'test_results': tv_test_results, 'ho_results': tv_ho_resutls, 'train_results': tv_train_results}
+            results_dict[discovery_rate][alpha] = {'test_results': tv_test_results,  'train_results': tv_train_results}
+        print(json.dumps(results_dict[discovery_rate], indent=4))
     
-    with open(results_dir / 'metrics.json' , 'w') as json_file:
-        json.dump(results_dict, json_file, indent=4)
+    # with open(results_dir / 'metrics.json' , 'w') as json_file:
+    #     json.dump(results_dict, json_file, indent=4)
     
     # print(results_dict)
     
