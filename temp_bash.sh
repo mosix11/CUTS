ab51437d95885b8160d86153388955f36d0e3a21
mosix11 <mosix11@yahoo.com>
2025-09-24 14:57:08 +0200

    fix in dion poison script

--- temp_bash.sh
+++ temp_bash.sh
@@ -1,6 +1,6 @@
 
-for x in {21..30}
+for x in {30..33}
 do
     echo "Running experiment with config$x"
-    CUDA_VISIBLE_DEVICES=1 python regular_noise_experiments.py -c config$x -f
+    python regular_noise_experiments.py -c config$x -f
 done
