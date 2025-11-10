e51ef09c837977acd5cb516d0b5a57eedd5e05c6
mosix11 <mosix11@yahoo.com>
2025-11-10 18:48:54 +0100

    -

--- m_bash_script.bash
+++ m_bash_script.bash
@@ -1,4 +1,4 @@
 for i in {21..42}; do
   echo "Running config$i..."
-  CUDA_VISIBLE_DEVICES=1 python run_experiment.py -e noise -a regular -c config$i
+  CUDA_VISIBLE_DEVICES=1 python run_experiment.py -e noise -a regular -c config$i -s
 done
